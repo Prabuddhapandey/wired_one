54595d5561766ede2e80e8ae647ae49b0fc087a2
Prabuddhapandey <prabuddharajpandey@gmail.com>
2025-02-18 22:11:27 +0545

    changes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\HomeComponent;
+use App\Livewire\UserComponent;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', HomeComponent::class)->name('home');
+Route::get('/user' , UserComponent::class)->name('user');
